b4c450b89d4bb3c7eb7703a9687cb7007b8bcfde
Deepika Palakonda <60010509+deepikap625@users.noreply.github.com>
2025-06-24 08:30:29 +0530

    Time: 2 ms (43.03%) | Memory: 42 MB (28.20%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -5,13 +5,15 @@ class Solution {
             travelDays.add(day);
         }
 
-        int maxDay = days[days.length - 1];
-        int[] dp = new int[maxDay + 1];
+        int maxDay = days[days.length - 1]; // last travel day
+        int[] dp = new int[maxDay + 1]; // minimum cost up to each day
 
         for (int day = 1; day <= maxDay; day++) {
             if (!travelDays.contains(day)) {
+                 // if the day is not a travel day then, cost is same as previous day
                 dp[day] = dp[day - 1];
-            } else {
+            } 
+            else {
                 int cost1 = dp[Math.max(0, day - 1)] + costs[0];
                 int cost7 = dp[Math.max(0, day - 7)] + costs[1];
                 int cost30 = dp[Math.max(0, day - 30)] + costs[2];
