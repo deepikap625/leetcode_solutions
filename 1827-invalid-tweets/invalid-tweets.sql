d685327e123c89bbf41896e042ec9cec7942a67a
Deepika Palakonda <60010509+deepikap625@users.noreply.github.com>
2025-09-09 21:38:26 +0530

    Time: 547 ms (86.74%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select tweet_id from Tweets where char_length(content) > 15 OR content regexp '[^a-zA-Z0-9!\' ]';
+select tweet_id from Tweets where char_length(content) > 15 OR content regexp '[^a-zA-Z0-9! ]';
